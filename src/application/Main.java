5fd26ac122cb3e941e0e55952068ab9300202a02
dmln <none>
2022-11-25 18:43:16 -0700

    created welcome scene and controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,24 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import java.io.FileInputStream;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/WelcomeScene.fxml"));
+			
+			Scene scene = new Scene(root,400,200);
+			
+			primaryStage.setTitle("Fitness Friend");
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
---
+++ src/application/WelcomeSceneController.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class WelcomeSceneController {
+
+    @FXML
+    private Button newProfileButton;
+
+    @FXML
+    private Button oldProfileButton;
+    
+    @FXML
+    private Button guestProfileButton;
+
+    @FXML
+    void createProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadProfile(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void bypassLogin(ActionEvent event) {
+    	
+    }
+
+}
