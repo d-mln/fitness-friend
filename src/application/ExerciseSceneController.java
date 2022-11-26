87d40bc893fefe5a27ce3880980bfb45096b5988
dmln <none>
2022-11-25 22:23:07 -0700

    exercise scene created

---
+++ src/application/ExerciseSceneController.java
@@ -0,0 +1,51 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ExerciseSceneController {
+
+    @FXML
+    private Label bikingUnitField;
+
+    @FXML
+    private TextField exerciseBikingField;
+
+    @FXML
+    private Button exerciseReturnWOSaving;
+
+    @FXML
+    private TextField exerciseRunningField;
+
+    @FXML
+    private Button exerciseSaveAndReturn;
+
+    @FXML
+    private TextField exerciseWalkingField;
+
+    @FXML
+    private TextField exerciseWorkingOutField;
+
+    @FXML
+    private Label runningUnitField;
+
+    @FXML
+    private Label swimmingUnitField;
+
+    @FXML
+    private Label walkingUnitField;
+
+    @FXML
+    void returnWOSavingPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAndReturnPressed(ActionEvent event) {
+
+    }
+
+}
