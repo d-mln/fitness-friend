8380da3cba3d982595c96e457d0e036c4fb25f22
dmln <none>
2022-11-25 21:43:23 -0700

    weight input scene added

---
+++ src/application/WeightSceneController.java
@@ -0,0 +1,38 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WeightSceneController {
+
+    @FXML
+    private Label unitLabel;
+
+    @FXML
+    private Label weightHistoryOne;
+
+    @FXML
+    private Label weightHistoryThree;
+
+    @FXML
+    private Label weightHistoryTwo;
+
+    @FXML
+    private Button weightReturnWOSaving;
+
+    @FXML
+    private Button weightSaveAndReturn;
+
+    @FXML
+    void returnWOSavingPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAndReturnPressed(ActionEvent event) {
+
+    }
+
+}
